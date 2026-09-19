b44a18b58b86840c4a79c98e3cb72ffe704c0df2
ryoworks16-crypto <ryo.works16@gmail.com>
2026-09-19 13:00:04 +0900

    村防衛戦を専用横長フィールドに変更、タイトルBGMの自動再生制限対策

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789789709|44767145';
+const CACHE_VERSION = '1789790072|33542477';
 /** @type {string} */
 const CACHE_PREFIX = 'HEARTH & HORDE-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
