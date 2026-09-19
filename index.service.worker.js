fd25887c9c0a1ddba53669b59904a51649908787
ryoworks16-crypto <ryo.works16@gmail.com>
2026-09-19 12:49:15 +0900

    村防衛戦をタワーディフェンスらしく刷新（2レーン・2拠点・柵）

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789781299|54532706';
+const CACHE_VERSION = '1789789709|44767145';
 /** @type {string} */
 const CACHE_PREFIX = 'HEARTH & HORDE-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
