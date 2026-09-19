b12ce68ab443d3ef88c979a01a82e8512f997a5c
ryoworks16-crypto <ryo.works16@gmail.com>
2026-09-19 10:07:04 +0900

    UIはみ出し修正・タッチ操作の当たり判定修正・自動UIチェック追加

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789777582|30618652';
+const CACHE_VERSION = '1789779988|30565506';
 /** @type {string} */
 const CACHE_PREFIX = 'HEARTH & HORDE-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
